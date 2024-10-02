4fe7af82632b692d864117e1ae26cc377465f62a
NikolayMesh <partizan.91@bk.ru>
2024-10-02 20:16:21 +0300

    add+

--- module_6_3.py
+++ module_6_3.py
@@ -1,28 +1,28 @@
 class Horse:
     def __init__(self):
-        self.x_distance = 0
-        self.sound = 'Frrr'
+        self.x_distance = 0 #пройденный путь.
+        self.sound = 'Frrr' #звук, который издаёт лошадь.
 
     def run(self, dx):
-        self.x_distance = int(self.x_distance) + int(dx)
+        self.x_distance = int(self.x_distance) + int(dx) # движение
 
 class Eagle:
     def __init__(self):
-        self.y_distance = 0
-        self.sound = 'I train, eat, sleep, and repeat'
+        self.y_distance = 0 # высота полёта
+        self.sound = 'I train, eat, sleep, and repeat' # звук, который издаёт орёл (отсылка)
 
     def fly(self, dy):
-        self.y_distance = int(self.y_distance) + int(dy)
+        self.y_distance = int(self.y_distance) + int(dy) # полёт
 
 
 class Pegasus(Horse, Eagle):
     def __init__(self):
-        Horse.__init__(self)
-        Eagle.__init__(self)
+        Horse.__init__(self) # наследование от коня
+        Eagle.__init__(self) # наследование от орла
 
     def move(self, dx, dy):
-        super().run(dx)
-        super().fly(dy)
+        super().run(dx) #пройденный путь.
+        super().fly(dy) # высота полёта
 
     def get_pos(self):
         return self.x_distance, self.y_distance
